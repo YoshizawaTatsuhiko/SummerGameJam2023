53e699ac8209088fb08f3d2fcf53f3558eb48fbf
UmenoKoudai <106291399+UmenoKoudai@users.noreply.github.com>
2023-09-26 13:36:24 +0900

    WallBaseの仕様変更 ボタンを押したときの仕組みと成功失敗の判定を別にした

--- Assets/Umeno/AllHitWall.cs
+++ Assets/Umeno/AllHitWall.cs
@@ -4,10 +4,22 @@ public class AllHitWall : WallBase
 {
     [SerializeField]int _clearCount = 3;
     int _hitCount = 0;
-
-    public int HitCount { get => _hitCount; set => _hitCount = value; }
     public override bool Judge()
     {
+
         return _clearCount == _hitCount;
+    }
+
+    public override void WallAction()
+    {
+        _hitCount++;
+    }
+
+    private void Update()
+    {
+        if(Judge())
+        {
+            Debug.Log("”j‰ó");
+        }
     }
 }
