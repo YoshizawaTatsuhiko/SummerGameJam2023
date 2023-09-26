7493aef19049735be6fc19b8f3c902d3d076c0b8
mukoyosihirota <mukohirotama8@gmail.com>
2023-09-26 14:48:51 +0900

    スクリプトを見やすく

--- Assets/Mukoyoshi/Menu.cs
+++ Assets/Mukoyoshi/Menu.cs
@@ -1,13 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
-
-
     public void GoScene(string name)
     {
         SceneManager.LoadScene(name);
